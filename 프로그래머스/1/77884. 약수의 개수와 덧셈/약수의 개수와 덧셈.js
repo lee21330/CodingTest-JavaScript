90f4e64a3d91da6c5b49dea89d56dbd5d76fd2e2
lee21330 <81613459+lee21330@users.noreply.github.com>
2024-12-13 16:40:34 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 5.70 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,18 @@
+function solution(left, right) {
+    var answer = 0;
+    var count = 0;
+    for (let i = left; i<=right; i++){
+        for (let j = 1; j<=i; j++){
+            if(i%j===0){
+                count += 1;
+            }
+        }
+        if(count%2 === 0){
+            answer = answer+i;
+        }else{
+            answer = answer-i;
+        }
+        count = 0;
+    }
+    return answer;
+}
