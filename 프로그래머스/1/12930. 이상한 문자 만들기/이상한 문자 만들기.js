e723a3d841bdc578c746139bee61457371d0e353
lee21330 <81613459+lee21330@users.noreply.github.com>
2024-12-20 02:31:52 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,23 +1,15 @@
 function solution(s) {
     var arr = s.split('');
-    var index = 0;
+    var index = 0; 
     
     for(let i = 0; i < arr.length; i++){
-        // 새로운 단어일때 index를 0으로 초기화
-        if (arr[i] == ' '){
+        if (arr[i] == ' '){ // 공백일때 index 초기화
             index = 0;
         }else{
-            // 단어의 짝수번째 알파벳은 대문자로
-            if(index % 2 === 0){
-                arr[i] = arr[i].toUpperCase();
-            // 단어의 홀수번째 알파벳은 소문자로
-            }else{
-                arr[i] = arr[i].toLowerCase();
-            }
+            arr[i] = index % 2 == 0?arr[i].toUpperCase():arr[i].toLowerCase();
             index++;
         }
-        
     }
-
+    
     return arr.join('');
 }
