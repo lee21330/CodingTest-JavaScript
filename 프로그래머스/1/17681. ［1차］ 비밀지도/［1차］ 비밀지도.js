2f4275833aee9b905a4ecba699fc57ba716d4a75
lee21330 <81613459+lee21330@users.noreply.github.com>
2025-01-05 23:18:06 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,22 +1,18 @@
 function solution(n, arr1, arr2) {
     var answer = [];
-    var array = [];
-    var string = '';
-    for(let i = 0; i < arr1.length; i++){
-        array.push([arr1[i].toString(2).padStart(n,0),arr2[i].toString(2).padStart(n,0)]);
-    }
 
-    for(let i = 0; i < n; i++){
+    for(let i = 0; i < arr1.length; i++){
+        const array = (arr1[i] | arr2[i]).toString(2).padStart(n,0);
+        var string = '';
         for(let j = 0; j < n; j++){
-            if(array[i][0][j] == 1 || array[i][1][j] == 1){
+            if(array[j] == 1){
                 string += '#';
             }else{
                 string += ' ';
             }
         }
-        
         answer.push(string);
-        string = '';
     }
+
     return answer;
 }
