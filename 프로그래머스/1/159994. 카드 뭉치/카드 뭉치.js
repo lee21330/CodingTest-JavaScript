05613d153e0c94c32a027b478fb964ca163d6415
lee21330 <81613459+lee21330@users.noreply.github.com>
2025-01-07 00:32:05 +0900

    [level 1] Title: 카드 뭉치, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+    for(let i = 0; i < goal.length; i++){
+        if(goal[i] != cards1[0] && goal[i] != cards2[0]){ 
+            return 'No' 
+        }else if(goal[i] == cards1[0]){ 
+            cards1.shift(); 
+        }else if(goal[i] == cards2[0]){ 
+            cards2.shift(); 
+        }
+    }
+    return 'Yes';
+}
