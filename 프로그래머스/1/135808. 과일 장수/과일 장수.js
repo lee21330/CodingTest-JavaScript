5c018e11be9f18a72569930649f9a8052fb381b3
lee21330 <81613459+lee21330@users.noreply.github.com>
2025-01-25 23:08:14 +0900

    [level 1] Title: 과일 장수, Time: 227.97 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,10 +1,13 @@
 function solution(k, m, score) {
     var answer = 0;
+    // 점수를 내림차순으로 정렬
     score.sort((a,b) => b-a);
     
+    // m개씩 그룹을 나눈 후 각 그룹의 최소점수 선택
     for(let i = m-1; i < score.length-(score.length % m); i += m){
         answer += score[i];
     }
     
+    // 최소점수 합 * 한 상자에 담긴 사과 개수
     return answer*m; 
 }
