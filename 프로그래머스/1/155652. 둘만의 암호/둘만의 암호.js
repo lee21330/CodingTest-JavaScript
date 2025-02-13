c0493b5a8e7218a35fe83703e3bf0589e8fbf1de
lee21330 <81613459+lee21330@users.noreply.github.com>
2025-02-13 11:56:09 +0900

    [level 1] Title: 둘만의 암호, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,13 +1,16 @@
 function solution(s, skip, index) {
     var answer = '';
-    // skip할 알파벳 제외
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('').filter((i)=>!skip.includes(i));
+    // skip에 포함된 알파벳 제외
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split('').filter((i)=>!skip.includes(i));
+    var idx = 0;
     
     for(let i = 0; i < s.length; i++){
-        var idx = alphabet.indexOf(s[i]);
-        for(let j = 0; j < index; j++){
-            idx++;
-            if(idx == alphabet.length) idx = 0;
+        idx = alphabet.indexOf(s[i]); // 현재 문자가 alphabet에서 위치한 인덱스
+        for(let j = 0; j < index; j++){ 
+            idx++; // 주어진 index만큼 이동
+            if(idx == alphabet.length){
+                idx = 0; // 범위를 벗어나면 0으로 돌아감
+            } 
         }
         answer += alphabet[idx];
     }
