e3c8f0a56ad11fdad2cf17819271e7c19f12dc5f
lee21330 <81613459+lee21330@users.noreply.github.com>
2025-02-24 18:35:00 +0900

    [level 1] Title: 숫자 짝꿍, Time: 1833.66 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,24 @@
+function solution(X, Y) {
+    var answer = '';
+    // X와 Y를 배열로 변환
+    var array_x = X.split('');
+    var array_y = Y.split('');
+    
+    // 9부터 0까지 공통된 숫자 찾기
+    for(let i = 9 ; i >= 0 ; i --) {
+        // X에서 숫자 i 횟수 구하기
+        const countX = array_x.filter(num => Number(num) === i).length;
+        // Y에서 숫자 i 횟수 구하기
+        const countY = array_y.filter(num => Number(num) === i).length;
+        // X와 Y에서 공통으로 등장하는 숫자의 최소 개수를 answer에 더하기
+        answer+=String(i).repeat(Math.min(countX, countY));
+    }
+    
+    // 공통된 숫자가 없는 경우
+    if(answer === '') return "-1"
+    
+    // answer가 0만 포함하는 경우
+    if(Number(answer) === 0) return "0"
+
+    return answer;
+}
